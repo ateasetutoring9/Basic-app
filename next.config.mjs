9e31702cbc6c5e15022e4269e82d979760a5a72f
HM <ateasetutoring9@gmail.com>
2026-05-09 12:11:31 +0800

    next config for Senetry has been update to fit Cloudflare deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,39 +7,24 @@ const nextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "at-ease-tutoring",
-
   project: "prod",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  // Upload a larger set of source maps for prettier stack traces
   widenClientFileUpload: true,
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
+  // Route browser requests to Sentry through Next.js to avoid ad-blockers
   tunnelRoute: "/monitoring",
 
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
+  // Disable automatic route-handler wrapping — next-on-pages cannot process
+  // the duplicate identifiers Sentry injects during instrumentation.
+  // Error capture is handled manually via instrumentation.ts + onRequestError.
+  autoInstrumentServerFunctions: false,
+  autoInstrumentMiddleware: false,
 
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  },
+  // Remove Sentry logger statements from the bundle
+  disableLogger: true,
 });
